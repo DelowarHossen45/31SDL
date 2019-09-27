120a44d7e1b0e5d39c5525b54a533fd2ea846520
Delowar Hossen Apple <applecse17@gmail.com>
2019-09-27 21:18:27 +0600

    complete

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.myapplication;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.calculator", appContext.getPackageName());
+        assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
